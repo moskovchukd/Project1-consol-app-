97c79ecf1f3534f85e1fc39ab00bd5347aa652e4
Danya <danylomoskovchuk@gmail.com>
2024-01-18 20:25:59 +0100

    Genres

--- js/script.js
+++ js/script.js
@@ -1,16 +1,64 @@
-const numberOfFilms = + prompt("How many films have you already watched?");
+"use strict";
+let numberOfFilms;
+
+ function start(){  
+    while(numberOfFilms =='' || numberOfFilms == null || isNaN(numberOfFilms)){
+        numberOfFilms = prompt("How many films have you already watched?");
+    }
+}
+start();
+
 let personalMovieDB = {
     count : numberOfFilms,
     movies: {},
     actors: {},
-    genres: {},
-    privat: false
+    genres: [],
+    privat: true
 };
-let i;
-for(i = 0; i < 2; i++){
-    let filmName = prompt("What is your last watched film?"),
-        filmRate = prompt("How would you rate it?");
-    personalMovieDB.movies[filmName] = filmRate;
+
+function rememberMyFilms() {
+    for(let i = 0; i < 2; i++){
+        let filmName = prompt("What is your last watched film?"),
+            filmRate = prompt("How would you rate it?");
+            if(filmName == null || filmName.length > 50 || filmRate == null || filmName == "" || filmRate == ""){
+                console.log("error!");
+                i--;
+            } else{
+                personalMovieDB.movies[filmName]=filmRate;
+            } 
+    }    
 }
 
-console.log(personalMovieDB);
+rememberMyFilms();
+
+function detectPersonalLevel(){
+    if(personalMovieDB.count < 10){
+        alert("You have watched a few films");
+    }  else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+        alert("You are an average film-watcher");
+    } else if (personalMovieDB.count >= 30){
+        alert('You are a real cinema worm');
+    } else {
+        alert("An error has occured!");
+    }
+}
+detectPersonalLevel();
+
+function writeYourGenres(){
+    let genre;
+    for(let i = 1; i <= 3; i++ ){
+        genre = prompt(`Your favourite genre number ${i}`);
+        personalMovieDB.genres.push(genre);
+    }
+}
+writeYourGenres();
+
+function showMyDB(){
+    if(personalMovieDB.privat){
+        return
+    } else{ 
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB();
